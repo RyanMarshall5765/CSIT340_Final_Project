d2b4b72e71c8f44d957db7544a1c4e77783ce006
RyanMarshall5765 <RyanMarshall5765@gmail.com>
2018-11-20 20:35:16 -0500

    Add initial server code

---
+++ chat_room_server.py
@@ -0,0 +1,47 @@
+from socket import socket, gethostbyname, gethostname, SOCK_DGRAM, AF_INET
+from threading import Thread
+from queue import Queue
+
+HOST = gethostbyname(gethostname())
+PORT = 5000
+ADDRESS = (HOST, PORT)
+BUFFSIZE = 4096
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(ADDRESS)
+
+
+def recvieve_msg(msg_socket, recv_msg):
+    while True:
+        msg, user = msg_socket.recvfrom(BUFFSIZE)
+        recv_msg.put((msg, user))
+
+
+def start_server():
+    print('Host IP: '+str(HOST))
+    clients = set()
+    recv_msg = Queue()
+
+    print('Server Running...')
+
+    Thread(target=recvieve_msg, args=(s, recv_msg)).start()
+
+    while True:
+        while not recv_msg.empty():
+            msg, user = recv_msg.get()
+            if user not in clients:
+                clients.add(user)
+                continue
+            clients.add(user)
+            msg = msg.decode('utf-8')
+            if msg.endswith('leave'):
+                clients.remove(user)
+                continue
+            print(str(user)+msg)
+            for client in clients:
+                if client != user:
+                    s.sendto(msg.encode('utf-8'), client)
+    s.close()
+
+
+if __name__ == '__main__':
+    start_server()
